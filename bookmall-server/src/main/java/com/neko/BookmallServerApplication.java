a76ea22ec3e75a4ad10e061e4922168de719e19d
wtcpython <1762993226@qq.com>
2025-11-22 22:06:37 +0800

    Spring Init

---
+++ bookmall-server/src/main/java/com/neko/BookmallServerApplication.java
@@ -0,0 +1,13 @@
+package com.neko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmallServerApplication.class, args);
+    }
+
+}
---
+++ bookmall-server/src/test/java/com/neko/BookmallServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmallServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
