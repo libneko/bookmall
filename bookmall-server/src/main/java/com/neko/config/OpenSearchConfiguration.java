15b61489766964911a9423cc139a1c0b698efbc3
wtcpython <1762993226@qq.com>
2025-11-29 14:21:40 +0800

    Ignore ssl check

--- bookmall-server/src/main/java/com/neko/config/OpenSearchConfiguration.java
+++ bookmall-server/src/main/java/com/neko/config/OpenSearchConfiguration.java
@@ -3,7 +3,11 @@ package com.neko.config;
 import org.apache.hc.client5.http.auth.AuthScope;
 import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
 import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.client5.http.ssl.TrustAllStrategy;
 import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestClientBuilder;
 import org.opensearch.client.RestHighLevelClient;
@@ -11,7 +15,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.net.ssl.SSLContext;
 import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class OpenSearchConfiguration {
@@ -26,17 +34,29 @@ public class OpenSearchConfiguration {
     private String password;
 
     @Bean
-    public RestHighLevelClient restHighLevelClient() throws URISyntaxException {
-        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    public RestHighLevelClient restHighLevelClient()
+            throws URISyntaxException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadTrustMaterial(new TrustAllStrategy())
+                .build();
+
+        var tlsStrategy = new DefaultClientTlsStrategy(sslContext, (host, session) -> true);
+
+        var cm = PoolingAsyncClientConnectionManagerBuilder.create()
+                .setTlsStrategy(tlsStrategy)
+                .build();
+
+        // 认证
+        var credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(
                 new AuthScope(null, -1),
-                new UsernamePasswordCredentials(username, password.toCharArray())
-        );
+                new UsernamePasswordCredentials(username, password.toCharArray()));
 
+        // 构建 RestClient
         RestClientBuilder builder = RestClient.builder(HttpHost.create(opensearchUrl))
-                .setHttpClientConfigCallback(httpClientBuilder ->
-                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
-                );
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
+                        .setDefaultCredentialsProvider(credentialsProvider)
+                        .setConnectionManager(cm));
 
         return new RestHighLevelClient(builder);
     }
